dd25ddfc909a3d8966699c108f1544cbb47fac6d
ryannguyen16 <98357619+ryannguyen16@users.noreply.github.com>
2022-07-28 09:08:41 -0700

    Update RetrieveScanformList.cs

--- official/docs/csharp/scanform/RetrieveScanformList.cs
+++ official/docs/csharp/scanform/RetrieveScanformList.cs
@@ -5,3 +5,5 @@ EasyPost.ClientManager.SetCurrent("EASYPOST_API_KEY");
 ScanFormCollection scanFormCollection = await ScanForm.All(new Dictionary<string, object>(){
   { "page_size", 5 }
 });
+
+Console.Write(scanForm);
