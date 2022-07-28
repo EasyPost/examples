d40f9530ae0894445e059a9eaecccb8593e3f916
ryannguyen16 <98357619+ryannguyen16@users.noreply.github.com>
2022-07-28 09:26:29 -0700

    Update InsureShipment.cs

--- official/docs/csharp/shipments/InsureShipment.cs
+++ official/docs/csharp/shipments/InsureShipment.cs
@@ -3,6 +3,7 @@ using EasyPost;
 EasyPost.ClientManager.SetCurrent("EASYPOST_API_KEY");
 
 Shipment purchasedShipment = await Shipment.Retrieve("shp_...");
+
 await purchasedShipment.Insure(888.50);
 
 Console.Write(purchasedShipment);
