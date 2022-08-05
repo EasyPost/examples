693dd9164c5bf4febe1297a4b6d43ef400bc6a61
ryannguyen16 <98357619+ryannguyen16@users.noreply.github.com>
2022-08-05 11:45:36 -0700

    Update DeletePaymentMethod.java

--- official/docs/java/billing/DeletePaymentMethod.java
+++ official/docs/java/billing/DeletePaymentMethod.java
@@ -3,9 +3,9 @@ import com.easypost.EasyPost;
 public class DeletePaymentMethod {
     public static void main(String[] args) {
         EasyPost.apiKey = System.getenv("EASYPOST_API_KEY");
-        
+
         boolean success = Billing.deletePaymentMethod(PaymentMethod.Priority.PRIMARY);
-    
+
         System.out.println(success);
     }
 }
