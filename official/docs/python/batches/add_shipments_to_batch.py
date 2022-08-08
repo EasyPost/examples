5aff954acad284b22c8ebe8f353725c3c3427029
ryannguyen16 <98357619+ryannguyen16@users.noreply.github.com>
2022-08-08 11:22:19 -0700

    Update add_shipments_to_batch.py

--- official/docs/python/batches/add_shipments_to_batch.py
+++ official/docs/python/batches/add_shipments_to_batch.py
@@ -5,9 +5,6 @@ easypost.api_key = "EASYPOST_API_KEY"
 
 batch = easypost.Batch.retrieve("batch_...")
 
-batch.add_shipments(shipments=[
-  {"id": "shp_..."},
-  {"id": "shp_..."},
-])
+batch.add_shipments(shipments=[{"id": "shp_..."}, {"id": "shp_..."}, ])
 
 print(batch)
