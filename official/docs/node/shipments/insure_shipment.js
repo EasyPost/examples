f8e8327d1adbd416c6481826959535591a5f4453
ryannguyen16 <98357619+ryannguyen16@users.noreply.github.com>
2022-08-12 13:01:38 -0700

    Update insure_shipment.js

--- official/docs/node/shipments/insure_shipment.js
+++ official/docs/node/shipments/insure_shipment.js
@@ -2,5 +2,5 @@ const Easypost = require('@easypost/api');
 const api = new Easypost('EASYPOST_API_KEY');
 
 api.Shipment.retrieve('shp_...').then((retrieveShipment) => {
-  retrieveShipment.insure(100).then(() => console.log(s));
+  retrieveShipment.insure(100).then(() => console.log(retrieveShipment));
 });
