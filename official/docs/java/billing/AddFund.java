3455fcaa6c7afc2ce2ae9d0a1dd2cde1d7b0bd86
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-09-23 14:31:12 -0600

    fix: payment method import

--- official/docs/java/billing/AddFund.java
+++ official/docs/java/billing/AddFund.java
@@ -3,12 +3,13 @@ package billing;
 import com.easypost.EasyPost;
 import com.easypost.exception.EasyPostException;
 import com.easypost.model.Billing;
+import com.easypost.model.PaymentMethod;
 
 public class AddFund {
     public static void main(String[] args) throws EasyPostException {
         EasyPost.apiKey = System.getenv("EASYPOST_API_KEY");
 
-        boolean success = Billing.fundWallet("2000", com.easypost.model.PaymentMethod.Priority.PRIMARY);
+        boolean success = Billing.fundWallet("2000", PaymentMethod.Priority.PRIMARY);
 
         System.out.println(success);
     }
