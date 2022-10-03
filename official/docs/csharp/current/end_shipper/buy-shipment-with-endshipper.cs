42c23f1b3f346b0fb132f9bc39c732ec97727144
Nate Harris <nwithan8@users.noreply.github.com>
2022-10-03 14:37:18 -0600

    - Fix EndShipper examples for C# (#35)

---
+++ official/docs/csharp/current/end_shipper/buy-shipment-with-endshipper.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EasyPost;
+using EasyPost.Models.API;
+using Newtonsoft.Json;
+
+namespace EasyPostExamples
+{
+    public class Examples
+    {
+        public static async Task Main()
+        {
+            string apiKey = Environment.GetEnvironmentVariable("EASYPOST_API_KEY")!;
+
+            var client = EasyPost.Client(apiKey);
+
+            Shipment shipment = await client.Shipment.Retrieve("shp_...");
+
+            Rate rate = shipment.LowestRate();
+
+            await shipment.Buy(rate, null, null, "es_...");
+
+            Console.WriteLine(JsonConvert.SerializeObject(shipment, Formatting.Indented));
+        }
+    }
+}
