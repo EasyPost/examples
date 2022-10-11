fc161b6677e15d12a7710abdc6e529d827750d43
jchen293 <jchen@easypost.com>
2022-10-11 12:14:27 -0400

    add spaces between JSON key and value

--- official/docs/curl/billing/create-easypost-credit-card.sh
+++ official/docs/curl/billing/create-easypost-credit-card.sh
@@ -3,7 +3,7 @@ curl -X POST "https://api.easypost.com/v2/credit_cards" \
   -H "Content-Type: application/json" \
   -d '{
     "credit_card": {
-      "stripe_object_id":"tok_...",
-      "priority":"primary"
+      "stripe_object_id": "tok_...",
+      "priority": "primary"
     }
   }'
