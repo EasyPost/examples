204e55bfce374e8d50eb5607b7c0363e656428c4
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-11-23 16:17:58 -0700

    docs: update snippets to retrieve child user API keys

--- official/docs/python/current/api-keys/retrieve.py
+++ official/docs/python/current/api-keys/retrieve.py
@@ -5,6 +5,11 @@ import easypost
 
 easypost.api_key = os.getenv("EASYPOST_API_KEY")
 
+# Retrieve all API keys (authenticated user and child user keys)
 api_keys = easypost.User.all_api_keys()
+
+# Retrieve API keys for a child user
+child_user = easypost.User.retrieve("user_...")
+api_keys = child_user.api_keys()
 
 print(api_keys)
