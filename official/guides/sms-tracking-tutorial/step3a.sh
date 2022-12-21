afeef804a9f1fb307a817616732ae0a26824a600
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-12-21 10:42:57 -0700

    fix: easypost url for sms guide

--- official/guides/sms-tracking-tutorial/step3a.sh
+++ official/guides/sms-tracking-tutorial/step3a.sh
@@ -1,1 +1,1 @@
-curl -X POST easypost.com/api/v2/webhooks -u "$EASYPOST_API_KEY": -d 'webhook[url]=http://xxxxxxxx.ngrok.io/easypost-webhook&webhook[mode]=test'
+curl -X POST https://api.easypost.com/v2/webhooks -u "$EASYPOST_API_KEY": -d 'webhook[url]=http://xxxxxxxx.ngrok.io/easypost-webhook&webhook[mode]=test'
