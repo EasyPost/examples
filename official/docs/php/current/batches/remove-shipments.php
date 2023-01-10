b2c4c353a15b83090cd1067c182b319f5f7419cb
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-01-10 13:25:35 -0700

    fix: removeShipments from batch

--- official/docs/php/current/batches/remove-shipments.php
+++ official/docs/php/current/batches/remove-shipments.php
@@ -4,10 +4,13 @@ $client = new \EasyPost\EasyPostClient(getenv('EASYPOST_API_KEY'));
 
 $batch = $client->batch->retrieve('batch_...');
 
-$batchWithoutShipments = $batch->removeShipments([
-    'shipments' => [
-        ['id' => 'shp_...']
+$batchWithoutShipments = $client->$batch->removeShipments(
+    $shipment->id,
+    [
+        'shipments' => [
+            ['id' => 'shp_...']
+        ]
     ]
-]);
+);
 
 echo $batchWithoutShipments;
