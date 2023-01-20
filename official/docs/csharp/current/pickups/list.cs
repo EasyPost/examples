8a7f9c65aaa5c9dbc8a43c6715bb2b7a18e7c35d
nharris <nwithan8@users.noreply.github.com>
2023-01-20 14:07:05 -0700

    - address feedback

--- official/docs/csharp/current/pickups/list.cs
+++ official/docs/csharp/current/pickups/list.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using EasyPost;
+using EasyPost.Models.API;
 
 namespace EasyPostExamples
 {
@@ -11,9 +13,14 @@ namespace EasyPostExamples
         {
             string apiKey = Environment.GetEnvironmentVariable("EASYPOST_API_KEY");
 
-            EasyPost.Client client = new EasyPost.Client(apiKey);
+            var client = new EasyPost.Client(apiKey);
 
-            EasyPost.Models.API.PickupCollection pickups = await client.Pickup.All();
+            Dictionary<string, object> listParams = new Dictionary<string, object>()
+            {
+                { "page_size", 5 }
+            };
+
+            PickupCollection pickups = await client.Pickup.All(listParams);
 
             Console.WriteLine(JsonConvert.SerializeObject(pickups, Formatting.Indented));
         }
