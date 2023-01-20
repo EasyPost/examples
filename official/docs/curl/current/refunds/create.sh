54167992194d12abd9f002b313786bb7102a0ddf
nharris <nwithan8@users.noreply.github.com>
2023-01-20 12:52:00 -0700

    - Move refunds/create to shipments/refund where it really belongs

--- official/docs/curl/current/refunds/create.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST https://api.easypost.com/v2/refunds \
-  -u "$EASYPOST_API_KEY": \
-  -H 'Content-Type: application/json' \
-  -d '{
-    "carrier": "USPS",
-    "tracking_codes": [
-      "EZ1000000001"
-    ]
-  }'
