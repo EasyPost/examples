8aeb9f100bc26580db56669549f299466cc0bef5
jchen293 <jchen@easypost.com>
2023-03-01 15:51:00 -0500

    add stateless rate for node

--- official/docs/node/current/rates/retrieve-stateless.js
+++ official/docs/node/current/rates/retrieve-stateless.js
@@ -1,1 +1,36 @@
-// Coming soon in V6 rewrite.
+const EasyPostClient = require('@easypost/api');
+
+const client = new EasyPostClient(process.env.EASYPOST_API_KEY);
+
+(async () => {
+  const rates = await client.Beta.Rate.retrieveStatelessRates({
+    to_address: {
+      name: 'Dr. Steve Brule',
+      street1: '179 N Harbor Dr',
+      city: 'Redondo Beach',
+      state: 'CA',
+      zip: '90277',
+      country: 'US',
+      email: 'dr_steve_brule@gmail.com',
+      phone: '4155559999',
+    },
+    from_address: {
+      street1: '417 montgomery street',
+      street2: 'FL 5',
+      city: 'San Francisco',
+      state: 'CA',
+      zip: '94104',
+      country: 'US',
+      company: 'EasyPost',
+      phone: '415-123-4567',
+    },
+    parcel: {
+      length: 20.2,
+      width: 10.9,
+      height: 5,
+      weight: 65.9,
+    },
+  });
+
+  console.log(rates);
+})();
