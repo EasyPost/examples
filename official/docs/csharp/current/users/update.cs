30f8416279f9c81d18cbf859679e2eb520902ad7
nwithan8 <nwithan8@users.noreply.github.com>
2023-05-11 15:10:49 -0600

    - All User examples

--- official/docs/csharp/current/users/update.cs
+++ official/docs/csharp/current/users/update.cs
@@ -18,10 +18,12 @@ namespace EasyPostExamples
 
             User user = await client.User.RetrieveMe();
 
-            await user.Update(new Dictionary<string, object>()
+            Parameters.User.Update parameters = new()
             {
-                { "recharge_threshold", 50.00 }
-            });
+                RechargeThreshold = "50.00"
+            };
+
+            user = await client.User.Update(user.Id, parameters);
 
             Console.WriteLine(JsonConvert.SerializeObject(user, Formatting.Indented));
         }
