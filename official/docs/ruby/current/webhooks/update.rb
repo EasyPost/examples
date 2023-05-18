6c17b3bb01d2bcb98a556fd014b54834a58a5c78
jchen293 <jchen@easypost.com>
2023-05-18 17:31:27 -0400

    minor fix on webhook update

--- official/docs/ruby/current/webhooks/update.rb
+++ official/docs/ruby/current/webhooks/update.rb
@@ -4,6 +4,6 @@ client = EasyPost::Client.new(api_key: ENV['EASYPOST_API_KEY'])
 
 retrieved_webhook = client.webhook.retrieve('hook_...')
 
-webhook.update(retrieved_webhook.id)
+webhook = client.webhook.update(retrieved_webhook.id)
 
 puts webhook
