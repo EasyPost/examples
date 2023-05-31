1e9a18ea36d17899112d81b1537a74db78481dd4
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-05-31 15:06:35 -0600

    fix: versions in curl

--- official/docs/curl/current/carrier-metadata/retrieve.sh
+++ official/docs/curl/current/carrier-metadata/retrieve.sh
@@ -1,7 +1,7 @@
 # Request all metadata for all carriers
-curl -X GET https://api.easypost.com/metadata/carriers \
+curl -X GET https://api.easypost.com/v2/metadata/carriers \
   -u "$EASYPOST_API_KEY":
 
 # Request specific metadata for specific carriers
-curl -X GET "https://api.easypost.com/metadata/carriers?carriers=usps&types=service_levels,predefined_packages" \
+curl -X GET "https://api.easypost.com/v2/metadata/carriers?carriers=usps&types=service_levels,predefined_packages" \
   -u "$EASYPOST_API_KEY":
