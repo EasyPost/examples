e1d4b9f6f4852aa95078d7d7c4c1c5faef212c9b
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-06-06 23:41:36 -0600

    fix: lint

--- official/docs/ruby/current/carrier-metadata/retrieve.rb
+++ official/docs/ruby/current/carrier-metadata/retrieve.rb
@@ -4,8 +4,8 @@ client = EasyPost::Client.new(api_key: ENV['EASYPOST_API_KEY'])
 
 # Request all metadata for all carriers
 carrier_metadata = client.carrier_metadata.retrieve
+puts carrier_metadata
 
 # Request specific metadata for specific carriers
-carrier_metadata = client.carrier_metadata.retrieve('usps', %w[service_levels predefined_packages])
-
-puts carrier_metadata
+usps_carrier_metadata = client.carrier_metadata.retrieve('usps', %w[service_levels predefined_packages])
+puts usps_carrier_metadata
