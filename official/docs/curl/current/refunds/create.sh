df947ee799779f2ea47bfbc3f7b87d0cbe5a5e69
Nate Harris <nwithan8@users.noreply.github.com>
2023-07-12 14:47:02 -0600

    [fix] Refund cURL example (#103)
    * - Fix refund cURL request
    
    * - Ignore CA .NET rule in style guide

--- official/docs/curl/current/refunds/create.sh
+++ official/docs/curl/current/refunds/create.sh
@@ -2,8 +2,10 @@ curl -X POST https://api.easypost.com/v2/refunds \
   -u "$EASYPOST_API_KEY": \
   -H 'Content-Type: application/json' \
   -d '{
-    "carrier": "USPS",
-    "tracking_codes": [
-      "EZ1000000001"
-    ]
+    "refund": {
+      "carrier": "USPS",
+      "tracking_codes": [
+        "EZ1000000001"
+      ]
+    }
   }'
