f66b5a6c2e7f52252672a4ab526277b6e9eae78c
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-08-30 16:37:41 -0600

    fix: retry example

--- official/guides/rate-limiting/retry_and_backoff.py
+++ official/guides/rate-limiting/retry_and_backoff.py
@@ -1,6 +1,7 @@
 import requests
 from urllib3.util.retry import Retry
 
+
 retry_strategy = Retry(
     total=3,
     backoff_factor=1,
@@ -11,12 +12,11 @@ retry_strategy = Retry(
         503,
         504,
     ],
-    method_whitelist=[
+    allowed_methods=[
         "DELETE",
         "GET",
     ],
 )
-adapter = requests.adapters.HTTPAdapter(max_retries=retry_strategy)
 requests_session = requests.Session()
 requests_http_adapter = requests.adapters.HTTPAdapter(max_retries=retry_strategy)
 requests_session.mount(prefix="https://api.easypost.com", adapter=requests_http_adapter)
