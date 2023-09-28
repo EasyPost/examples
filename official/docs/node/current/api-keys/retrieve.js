bd3e9ec808c7b7295c9b28c1c82472158220d138
Danilo Caballero Chinchilla <danilo@easypost.com>
2023-09-28 13:39:11 -0700

    Update location of call for Node

--- official/docs/node/current/api-keys/retrieve.js
+++ official/docs/node/current/api-keys/retrieve.js
@@ -3,13 +3,13 @@ const EasyPostClient = require('@easypost/api');
 const client = new EasyPostClient(process.env.EASYPOST_API_KEY);
 
 (async () => {
-  let apiKeys;
-
   // Retrieve all API keys including children
-  apiKeys = await client.ApiKey.all();
-
-  // Retrieve API keys for a specific child user
-  client.User.apiKeys('user_...').then((childApiKeys) => console.log(childApiKeys));
+  let apiKeys = await client.ApiKey.all();
 
   console.log(apiKeys);
+
+  // Retrieve API keys for a specific child user
+  let childApiKeys = await client.ApiKey.retrieveApiKeysForUser('user_...');
+
+  console.log(childApiKeys);
 })();
