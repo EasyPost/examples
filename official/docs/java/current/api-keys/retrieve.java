f81471100d6d56b28fd0297e9d25543124e5b582
Danilo Caballero Chinchilla <danilo@easypost.com>
2023-09-28 14:02:13 -0700

    Change method location in Java examples

--- official/docs/java/current/api-keys/retrieve.java
+++ official/docs/java/current/api-keys/retrieve.java
@@ -8,12 +8,12 @@ public class Retrieve {
     public static void main(String[] args) throws EasyPostException {
         EasyPostClient client = new EasyPostClient(System.getenv("EASYPOST_API_KEY"));
 
-        ApiKeys parentKeys = client.apikeys.all();
+        ApiKeys parentKeys = client.apiKey.all();
 
         System.out.println(parentKeys);
 
         // Retrieve API keys for a specific child user
-        ApiKeys childKeys = client.user.apiKeys("user_...");
+        ApiKeys childKeys = client.apiKey.retrieveApiKeysForUser("user_...");
 
         System.out.println(childKeys);
     }
