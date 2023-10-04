374c1c9407240bb7859173ef2132c51dfa4f4537
Danilo Caballero chinchilla <danilo@easypost.com>
2023-10-04 22:44:14 +0000

    Add curl example

---
+++ official/docs/curl/current/pagination/get-next-page.sh
@@ -0,0 +1,7 @@
+# Get first page of results
+curl -X GET https://api.easypost.com/v2/shipments?page_size=5 \
+  -u "$EASYPOST_API_KEY":
+
+# Provide the ID of the last element of the previous page in the before_id param
+curl -X GET https://api.easypost.com/v2/shipments?page_size=5&before_id=shp_... \
+  -u "$EASYPOST_API_KEY":
