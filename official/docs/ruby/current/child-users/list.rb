00b42a669171a869f5d27edb4aac35cafa91f3b3
nwithan8 <nwithan8@users.noreply.github.com>
2024-01-24 15:33:54 -0700

    - Ruby linting

--- official/docs/ruby/current/child-users/list.rb
+++ official/docs/ruby/current/child-users/list.rb
@@ -2,8 +2,8 @@ require 'easypost'
 
 client = EasyPost::Client.new(api_key: ENV['EASYPOST_API_KEY'])
 
-childUsers = client.user.all_children(
+child_users = client.user.all_children(
   page_size: 5,
 )
 
-puts childUsers
+puts child_users
