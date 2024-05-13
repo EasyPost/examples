d9115e663095e694da8374c0e6bd97211fd6bff1
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2024-05-13 11:40:31 -0600

    fix: fundWallet reference for Node docs

--- official/docs/node/v5/billing/fund.js
+++ official/docs/node/v5/billing/fund.js
@@ -2,4 +2,4 @@ const Easypost = require('@easypost/api');
 
 const api = new Easypost('EASYPOST_API_KEY');
 
-api.Billing.fund('2000', 'primary').then(console.log);
+api.Billing.fundWallet('2000', 'primary').then(console.log);
