f095e08d9e3761b83d528f1c802ccfb68bf9e081
Nate Harris <nwithan8@users.noreply.github.com>
2024-05-24 10:49:12 -0600

    - Fix smartrate function name in Ruby examples (#130)

--- official/docs/ruby/current/smartrate/retrieve-time-in-transit-statistics.rb
+++ official/docs/ruby/current/smartrate/retrieve-time-in-transit-statistics.rb
@@ -4,6 +4,6 @@ client = EasyPost::Client.new(api_key: 'EASYPOST_API_KEY')
 
 retrieved_shipment = client.shipment.retrieve('shp_...')
 
-shipment = client.shipment.get_smartrates(retrieved_shipment.id)
+shipment = client.shipment.get_smart_rates(retrieved_shipment.id)
 
 puts shipment
