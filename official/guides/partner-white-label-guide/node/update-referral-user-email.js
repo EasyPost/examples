ad43c813ac9ddca96f7025d7534d22121d21519a
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2024-06-04 13:49:51 -0600

    chore: remove unused partner-white-label-guide snippets

--- official/guides/partner-white-label-guide/node/update-referral-user-email.js
+++
@@ -1,4 +0,0 @@
-const Easypost = require('@easypost/api');
-const api = new Easypost('<YOUR_PRODUCTION_API_KEY>');
-
-api.Referral.updateEmail('user_...', 'new_email@example.com').then(console.log);
