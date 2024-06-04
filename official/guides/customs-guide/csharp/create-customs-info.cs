73d4b7ff32e3b7c653c8c1bcb14aa97b24a7d06c
Nate Harris <nwithan8@users.noreply.github.com>
2024-06-04 13:15:50 -0600

    - Remove, update examples for .NET

--- official/guides/customs-guide/csharp/create-customs-info.cs
+++
@@ -1,5 +0,0 @@
-CustomsInfo customsInfo = await CustomsInfo.Create(new Dictionary<string, object>() {
-  { "customs_certify", true }, { "eel_pfc", "NOEEI 30.37(a)" },
-  { "customs_signer", "Jarrett Streebin" }, { "contents_type", "gift" },
-  { "customs_items", new List<Dictionary<string, object>>() { customsItem1, customsItem2 } }
-});
