d1612ff73964cf5fbc94f0f40b754bc38d337116
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2024-06-04 11:05:01 -0600

    fix: errors-guide snippets

--- official/guides/errors-guide/ruby/catch-error.rb
+++ official/guides/errors-guide/ruby/catch-error.rb
@@ -1,7 +1,9 @@
 require 'easypost'
 
+client = EasyPost::Client.new(api_key: 'EASYPOST_API_KEY')
+
 begin
-  Address.create({}, strict_verify: true)
-rescue EasyPost::Errors::ApiError => e
-  p e.code
+  client.address.create({strict_verify: true})
+rescue EasyPost::Errors::Api::ApiError => e
+  puts e.code
 end
