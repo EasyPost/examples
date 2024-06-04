fbcf9aa07f00721ef5a36f8b048e02447bbce51f
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2024-06-04 11:09:06 -0600

    fix: landing page snippet

--- official/landing_pages/shipping_insurance/insure_shipment.py
+++ official/landing_pages/shipping_insurance/insure_shipment.py
@@ -1,10 +1,12 @@
-import easypost
 import os
+
+import easypost
+
 
 client = easypost.EasyPostClient(os.getenv("EASYPOST_API_KEY"))
 
 # Purchase and insure a shipment
-shipment = easypost.shipment.retrieve("shp_123")
+shipment = client.shipment.retrieve("shp_123")
 client.shipment.buy(
     shipment.id,
     rate=shipment.lowest_rate(),
