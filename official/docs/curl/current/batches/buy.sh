a694c8516ba578db8842d5aab01ec197636d5bf2
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2024-06-25 15:52:39 -0600

    fix: curl batch buy

--- official/docs/curl/current/batches/buy.sh
+++ official/docs/curl/current/batches/buy.sh
@@ -1,2 +1,18 @@
+curl -X POST https://api.easypost.com/v2/batches \
+  -u "EASYPOST_API_KEY": \
+  -H 'Content-Type: application/json' \
+  -d '{
+    "batch": {
+      "shipments": [
+        "from_address": {"id": "adr_..."},
+        "to_address": {"id": "adr_..."},
+        "parcel": {"id": "prcl_..."},
+        "service": "First",
+        "carrier": "USPS",
+        "carrier_accounts": ["ca_..."]
+      ]
+    }
+  }'
+
 curl -X POST https://api.easypost.com/v2/batches/batch_.../buy \
   -u "EASYPOST_API_KEY":
