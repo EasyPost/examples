d88ce340dc7913283e32df1db570b8e32eeba2b1
jchen293 <jchen@easypost.com>
2024-07-15 16:34:20 -0400

    fix lint for node

--- official/docs/node/current/smartrate/retrieve-standalone-smartrate-deliver-by.js
+++ official/docs/node/current/smartrate/retrieve-standalone-smartrate-deliver-by.js
@@ -3,11 +3,11 @@ const EasyPostClient = require('@easypost/api');
 const client = new EasyPostClient('EASYPOST_API_KEY');
 
 const params = {
-    from_zip: "10001",
-    to_zip: "10002",
-    planned_ship_date: "2024-07-18",
-    carriers: ["UPS"],
-  };
+  from_zip: '10001',
+  to_zip: '10002',
+  planned_ship_date: '2024-07-18',
+  carriers: ['UPS'],
+};
 
 (async () => {
   const results = await client.SmartRate.estimateDeliveryDate(params);
