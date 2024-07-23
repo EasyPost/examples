d930c7c4173ff507893dd8a656f9d8726f74d7a6
jchen293 <jchen@easypost.com>
2024-07-23 18:36:35 -0400

    lint

--- official/docs/node/current/claims/create.js
+++ official/docs/node/current/claims/create.js
@@ -5,15 +5,9 @@ const client = new EasyPostClient('EASYPOST_API_KEY');
 (async () => {
   const claim = await client.Claim.create({
     type: 'damage',
-    email_evidence_attachments: [
-      'REPLACE_WITH_BASE64_STRING',
-    ],
-    invoice_attachments: [
-      'REPLACE_WITH_BASE64_STRING',
-    ],
-    supporting_documentation_attachments: [
-      'REPLACE_WITH_BASE64_STRING',
-    ],
+    email_evidence_attachments: ['REPLACE_WITH_BASE64_STRING'],
+    invoice_attachments: ['REPLACE_WITH_BASE64_STRING'],
+    supporting_documentation_attachments: ['REPLACE_WITH_BASE64_STRING'],
     description: 'Test Description',
     contact_email: 'test@example.com',
     tracking_code: 'YOUR_TRACKING_CODE',
