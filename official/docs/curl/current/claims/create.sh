751daddf380352c55d0551cd53777716acdeaed1
Jack Chen <jchen@easypost.com>
2024-07-24 15:49:11 -0400

    Fix typo for claims create curl (#146)

--- official/docs/curl/current/claims/create.sh
+++ official/docs/curl/current/claims/create.sh
@@ -1,4 +1,4 @@
-curl -X POST https://api.easypost.com/v2/insurances \
+curl -X POST https://api.easypost.com/v2/claims \
   -u "EASYPOST_API_KEY": \
   -H 'Content-Type: application/json' \
   -d '{
