43eace62b1280f33cc2db118f150190cfbe7cd0c
Holly Abrams <habrams@easypost.com>
2025-04-01 15:34:15 -0400

    Update Amazon Shipping and Royal Mail v3 carrier curls

---
+++ official/guides/create-carrier-curls/royalmailv3.sh
@@ -0,0 +1,24 @@
+curl -X POST https://api.easypost.com/v2/carrier_accounts \
+  -u "$EASYPOST_API_KEY": \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "type": "RoyalMailV3Account",
+  "description": "RoyalMailV3Account",
+  "carrier_account": {
+    "credentials": {
+      "account_number": "VALUE",
+      "contact_name": "VALUE",
+      "contact_phone": "VALUE",
+      "dispatch_city": "VALUE",
+      "dispatch_country": "VALUE",
+      "dispatch_state": "VALUE",
+      "dispatch_street1": "VALUE",
+      "dispatch_street2": "VALUE",
+      "dispatch_zip": "VALUE",
+      "internal_location_id": "VALUE",
+      "oba_access_code": "VALUE",
+      "oba_email_address": "VALUE",
+      "posting_location_number": "VALUE"
+    }
+  }
+}'
