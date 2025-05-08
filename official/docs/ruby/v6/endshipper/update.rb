f7c43036ac2a89bdb04b8e298fbbd09c8db32f1d
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-05-08 14:33:25 -0600

    fix: another endshipper arrow

--- official/docs/ruby/v6/endshipper/update.rb
+++ official/docs/ruby/v6/endshipper/update.rb
@@ -5,15 +5,15 @@ client = EasyPost::Client.new(api_key: 'EASYPOST_API_KEY')
 end_shipper = client.end_shipper.update(
   'es_...',
   {
-    'name' => 'New Name',
-    'street1' => '388 Townsend St',
-    'street2' => 'Apt 20',
-    'city' => 'San Francisco',
-    'state' => 'CA',
-    'zip' => '94107',
-    'country' => 'US',
-    'email' => 'test@example.com',
-    'phone' => '5555555555',
+    name: 'New Name',
+    street1: '388 Townsend St',
+    street2: 'Apt 20',
+    city: 'San Francisco',
+    state: 'CA',
+    zip: '94107',
+    country: 'US',
+    email: 'test@example.com',
+    phone: '5555555555',
   },
 )
 
