c557a26389a589235d898905b2ca4c5b7e2b70b9
Holly Abrams <habrams@easypost.com>
2025-07-01 11:53:44 -0400

    Adding Node buy and one-call-buy examples

---
+++ official/docs/node/current/luma/buy.js
@@ -0,0 +1,10 @@
+const EasyPost = require("@easypost/api");
+
+const api = new EasyPost("EASYPOST_API_KEY");
+
+(async () => {
+  const shipment = await api.Shipment.retrieve("shp_...");
+  await shipment.buy(shipment.lowestRate());
+
+  console.log(shipment);
+})();
