1afc82b4e643ed52e0f5baae98f5d288626b9686
Justin Hammond <39606064+Justintime50@users.noreply.github.com>
2025-10-16 15:34:45 -0600

    fix: wrap the end shipper curl params (#214)

--- official/docs/curl/current/endshipper/create.sh
+++ official/docs/curl/current/endshipper/create.sh
@@ -2,16 +2,18 @@ curl -X POST https://api.easypost.com/v2/end_shippers \
   -u "EASYPOST_API_KEY": \
   -H 'Content-Type: application/json' \
   -d '{
-    "address": {
-      "name": "FOO BAR",
-      "company": "BAZ",
-      "street1": "164 TOWNSEND STREET UNIT 1",
-      "street2": "UNIT 1",
-      "city": "SAN FRANCISCO",
-      "state": "CA",
-      "zip": "94107",
-      "country": "US",
-      "phone": "555-555-5555",
-      "email": "FOO@EXAMPLE.COM"
+    "end_shipper": {
+      "address": {
+        "name": "FOO BAR",
+        "company": "BAZ",
+        "street1": "164 TOWNSEND STREET UNIT 1",
+        "street2": "UNIT 1",
+        "city": "SAN FRANCISCO",
+        "state": "CA",
+        "zip": "94107",
+        "country": "US",
+        "phone": "555-555-5555",
+        "email": "FOO@EXAMPLE.COM"
+      }
     }
   }'
