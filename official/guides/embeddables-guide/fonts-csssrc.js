e53421d867e7868d7f4d49cfc3801067d4c3bacb
Holly Abrams <habrams@easypost.com>
2025-10-27 19:13:56 -0400

    Add embeddables code examples

---
+++ official/guides/embeddables-guide/fonts-csssrc.js
@@ -0,0 +1,5 @@
+EasyPostEmbeddables.init({
+  fonts: [
+   { cssSrc: "https://fonts.googleapis.com/css2?family=Zen+Dots&display=swap" },
+  ],
+});
---
+++ official/guides/embeddables-guide/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import requests
+import os
+
+app = Flask(__name__)
+
+EASYPOST_API_KEY = os.environ.get("EASYPOST_API_KEY", "")
+
+@app.route("/api/easypost-embeddables/session", methods=["GET"])
+def create_embeddable_session():
+    payload = {
+        "user_id": <SUB_ACCOUNT_USER_ID>,
+        "origin_host": <ORIGIN_HOST>
+    }
+
+    response = requests.post(
+        "https://api.easypost.com/v2/embeddables/session",
+        auth=(EASYPOST_API_KEY, ""),
+        json=payload,
+        headers={"Content-Type": "application/json"},
+        timeout=10,
+    )
+
+    response.raise_for_status()
+    return jsonify(response.json())
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
