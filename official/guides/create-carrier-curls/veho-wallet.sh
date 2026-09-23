35e4c33e5857ecb76cff8115243154587b61a9ff
jhammond <jhammond@easypost.com>
2026-09-23 15:22:39 +0000

    Add Veho Wallet carrier account curl example

---
+++ official/guides/create-carrier-curls/veho-wallet.sh
@@ -0,0 +1,12 @@
+curl -X POST https://api.easypost.com/v2/carrier_accounts/open \
+	-u "$EASYPOST_API_KEY": \
+	-H 'Content-Type: application/json' \
+	-d '{
+  "carrier_account": {
+    "type": "VehoWalletAccount",
+    "reference": "Veho Wallet Account",
+    "description": "Veho Wallet Account",
+    "company": "VALUE",
+    "agreed_to_terms_at": "VALUE"
+  }
+}'
